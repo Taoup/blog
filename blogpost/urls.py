b1516982d2877a461227c4546b66a70b7ca6f04a
dawn <mahongtao0x7e0@163.com>
2019-11-28 20:33:04 +0800

    markdown ok

--- blogpost/urls.py
+++ blogpost/urls.py
@@ -4,5 +4,4 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('<int:postid>/', views.post_details, name='post_detail'),
 ]
--- blogpost/views.py
+++ blogpost/views.py
@@ -2,13 +2,20 @@ from django.shortcuts import render
 from .models import Blogpost
 from django.http import HttpResponse
 from django.shortcuts import render
+import markdown
+
 
 # Create your views here.
 def index(request):
     recent_20_posts = Blogpost.objects.order_by('-posted_date')[:20]
+    for post in recent_20_posts:
+        post.body = markdown.markdown(post.body,
+         extensions=[
+            # 包含 缩写、表格等常用扩展
+            'markdown.extensions.extra',
+            # 语法高亮扩展
+            'markdown.extensions.codehilite',
+            ]
+        )
     context = {'recent_20_posts': recent_20_posts}
     return render(request, 'blogpost/index.html', context)
-
-def post_details(request, postid):
-    post = Blogpost.objects.filter(pk = postid)
-    return HttpResponse(post.get().title)
